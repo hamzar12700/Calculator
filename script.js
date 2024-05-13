39a31451a4596f4cb2df66644a59d4eef8d23268
hamzar12700 <hamza2203c@aptechgdn.net>
2024-05-13 23:55:43 +0500

    Add Delete , Ac and Calculation

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-let display = document.getElementById("inputBox"); 
+let display = document.getElementById("inputBox");
 
 let button = document.querySelectorAll("button");
-
 let string = "";
-
 let buttonArray = Array.from(button);
 
-buttonArray.forEach((btn)=>{
-
-btn.addEventListener("click",(e)=>{
-//e.target.innerHTML;
-if(e.target.innerHTML == "DEL"){
-    string =  string.substring(0, string.length-1)
-}
-string += e.target.innerHTML;
-display.value = string;
-
-//console.log(e.target.innerHTML);
-});
-
-
-   // console.log(btn);
+buttonArray.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if ((e.target.innerHTML == "DEL")) {
+      string = string.substring(0, string.length - 1);
+      display.value = string;
+    }  
+   else if ((e.target.innerHTML == "AC")) {
+      string = "";
+      display.value = string;
+    }
+   else if ((e.target.innerHTML == "=")) {
+      string = eval(string);
+      display.value = string;
+    } else {
+      string += e.target.innerHTML;
+      display.value = string;
+    }
+  });
 });
